b220dbbbbf9c620563888a53d79525bb0797bcd9
Rafee <rafeehasan88@gmail.com>
2025-03-07 12:27:15 +0600

    slider added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Swiper from './Swiper';
+
+
+
 
 const Home = () => {
     return (
         <div>
             <h2>This is Home</h2>
+            <Swiper></Swiper>
+        
         </div>
     );
 };
---
+++ src/Components/Swiper.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Data from "../Data.json";
+const Swiper = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
+  return (
+    <div className="w-3/4 m-auto">
+      <div className="mt-20">
+      <Slider {...settings}>
+        {Data.map((e) => (
+          <div className="bg-white h-[300px] text-black rounded-xl">
+            <div className="h-56 rounded-t-xl bg-[#2e303d] flex justify-center items-center">
+              <img className="h-44 w-44" src={e.brand_logo} alt="" />
+            </div>
+            <div className="bg-[#2e303d] flex justify-center items-center gap-4 p-4">
+            <p className="text-lg font-bold bg-[#2e303d] text-white ">{e.brand_name}</p>
+            </div>
+          </div>
+        ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Swiper;
